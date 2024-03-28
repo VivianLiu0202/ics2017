a6fcd752d6859bafa444fb01e2f2ff65b585e0a9
tracer-ics2017 <tracer@njuics.org>
2024-03-28 00:45:37 -0700

    >  compile 2112614 vivian Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:45:37 up  1:49,  1 user,  load average: 2.89, 1.37, 0.78 274a099f6790fb3010f46dad5b35de6e39cda2d9

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -96,3 +96,14 @@ make_EHelper(not)
 
   print_asm_template1(not);
 }
+
+//pa3 level2 : add rol
+make_EHelper(rol)
+{
+  rtl_shri(&t2, &id_dest->val, id_dest->width * 8 - id_src->val);
+  rtl_shl(&t3, &id_dest->val, &id_src->val);
+  rtl_or(&t1, &t2, &t3);
+  operand_write(id_dest, &t1);
+
+  print_asm_template2(rol);
+}
