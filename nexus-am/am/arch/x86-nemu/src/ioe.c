bf8f00d88038378c71c94bc7c932b5c071c222c1
tracer-ics2017 <tracer@njuics.org>
2024-03-29 00:21:33 -0700

    >  run 2112614 vivian Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:21:33 up  1:20,  1 user,  load average: 0.77, 0.64, 0.89 b94b09e33e484fdd27b35c8fc2429864b198141d

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -25,12 +25,25 @@ _Screen _screen = {
 
 extern void *memcpy(void *, const void *, int);
 
+//pa2 level3: finish _draw_rect
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h)
 {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++)
+
+  // int i;
+  // for (i = 0; i < _screen.width * _screen.height; i++)
+  // {
+  //   fb[i] = i;
+  // }
+  int widthClamped = (w > _screen.width - x) ? _screen.width - x : w;
+  int copyBytesPerRow = sizeof(uint32_t) * widthClamped;
+  for (int row = 0; row < h && y + row < _screen.height; row++)
   {
-    fb[i] = i;
+    // Calculate destination pointer in the frame buffer
+    uint32_t *dest = &fb[(y + row) * _screen.width + x];
+    // Copy the pixel row from source to destination
+    memcpy(dest, pixels, copyBytesPerRow);
+    // Move the source pointer to the next row of pixels
+    pixels += w;
   }
 }
 
