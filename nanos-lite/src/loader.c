db5fedc1a0b57f0a1ff03d7e939bbf1800e1ed93
tracer-ics2017 <tracer@njuics.org>
2024-04-03 00:36:21 -0700

    >  compile 2112614 vivian Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:36:21 up  2:04,  2 users,  load average: 1.14, 1.01, 0.82 7c929155c7820283686fc953135bdb21de324a13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,13 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+extern size_t get_ramdisk_size();
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
 
-uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+uintptr_t loader(_Protect *as, const char *filename)
+{
+  //ramdisk_read函数：从ramdisk中offset偏移处的len字节读入到buf中
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
